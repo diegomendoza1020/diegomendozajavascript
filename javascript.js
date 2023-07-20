95d11dd6834392ff1d106f547cfc4cd66c56cc1c
diego <diegomendoza1020@gmail.com>
2023-07-20 04:24:46 -0300

    Proyecto final

--- javascript.js
+++ javascript.js
@@ -1,69 +1,218 @@
-var preguntas = [
-    {
-      pregunta: '¿Como se llamó el tour que marcó el regreso de Soda Stereo luego de "El Ultimo Concierto" en 1996? \n\n A) Me Veras Volver\n\n B) (En) El Septimo Dia\n\n C) Fuerza Natural ' ,
-      respuesta: "A"
+class Producto{
+  constructor(name, id, type, price){
+      this.name = name;
+      this.id = id;
+      this.type = type;
+      this.price = price;
+      
+      
+  }
+}
+
+
+// Array
+const ProductoDefault = [
+  {
+      name:"Cherry Glazerr Face T-Shirt", 
+      id:"001", 
+      type:"Clothes", 
+      price:25, 
+       
+      
+  },
+
+  {  
+     name:"Stuffed & Ready", 
+      id:"003",
+      type:"Discs", 
+      price:"9",
+     
+      
     },
-    {
-      pregunta: 'En 1975, la banda britanica Queen publicó una cancion que el paradigma de la musica en aquel entonces, ¿Como se llamó? \n\n A) Radio Rhapsody \n\n B) Queen Rhapsody \n\n C) Bohemian Rhapsody   ',
-      respuesta: "C"
-    },
-    {
-      pregunta: '¿Qué instrumento tocó Dave Grohl en Nirvana?\n\n A) Guitarra \n\n B) Bateria \n\n C) Bajo  ',
-      respuesta: "B"
-    }
-  ];
-  localStorage.setItem("preguntas", JSON.stringify(preguntas));
+
+  { 
+      name:"Apocalipstick", 
+      id:"004",
+      type:"Discs",
+      price:9, 
+      
+      },
 
 
-  var preguntasAlmacenadas = localStorage.getItem("preguntas");
-  
+ 
+]
+// local storage
+const productos = JSON.parse(localStorage.getItem("productos")) || [] 
+const compras = JSON.parse(localStorage.getItem("compras")) || []
+let carrito = JSON.parse(localStorage.getItem("carrito")) || []
 
-  if (preguntasAlmacenadas) {
-    preguntas = JSON.parse(preguntasAlmacenadas);
-  }
-
-  var indicePreguntaActual = 0;
-  var puntaje = 0;
-  
-
-  function mostrarPregunta() {
-    var preguntaElemento = document.getElementById("pregunta");
-    preguntaElemento.textContent = preguntas[indicePreguntaActual].pregunta;
-  }
-  
-
-  function comprobarRespuesta() {
-    var respuestaUsuario = document.getElementById("respuesta").value.toLowerCase();
-    var respuestaCorrecta = preguntas[indicePreguntaActual].respuesta.toLowerCase();
-    var resultadoElemento = document.getElementById("resultado");
-  
-    if (respuestaUsuario === respuestaCorrecta) {
-      resultadoElemento.textContent = "¡Muy bien!";
-      puntaje++;
+const selectorTipo = document.getElementById("tipoProducto")
+selectorTipo.onchange = (evt)=>{
+    const tipoSeleccionado =  evt.target.value
+    if(tipoSeleccionado === "0"){
+        generarProductos(productos)
     } else {
-      resultadoElemento.textContent = "¡Incorrecto, la respuesta correcta es  " + respuestaCorrecta + "!";
+        generarProductos(productos.filter(prod=>prod.type === tipoSeleccionado))
     }
+}
+
+const agregarProductos = ({name, id, type, price})=>{
+
+  if(productos.some(prod=>prod.id===id)){
+  } 
   
-   
-    if (indicePreguntaActual < preguntas.length - 1) {
-      indicePreguntaActual++;
-      mostrarPregunta();
-    } else {
-      resultadoElemento.textContent = "Juego terminado. Puntaje total: " + puntaje + " de " + preguntas.length;
-      document.getElementById("respuesta").disabled = true;
-      document.getElementById("botonResponder").disabled = true;
-    }
+  else {
+      const productoNuevo = new Producto(name, id, type, price)
+      productos.push(productoNuevo)
+      localStorage.setItem('productos', JSON.stringify(productos))
   }
-  
-  
-  var botonResponder = document.getElementById("botonResponder");
-  
+}
 
-  botonResponder.addEventListener("click", comprobarRespuesta);
-  
+const agregarCarrito = (objetoCarrito)=>{
 
-  mostrarPregunta();
-  
+  carrito.push(objetoCarrito)
 
+  totalDelCarritoMostrar ()
   
+}
+const mostrarCarrito = ()=>{
+  const listadoCarrito = document.getElementById("listadoCarrito")
+  listadoCarrito.innerHTML=""
+  carrito.forEach(({name, price, quantity, id}) =>{
+      let elementoLista = document.createElement("li")
+      elementoLista.innerHTML=` <h6> ${name} (${quantity}) </h6>    $${price}    <button id ="eliminarCarrito${id}" class="boton-limpiarCarrito btn">X</button>`
+      listadoCarrito.appendChild(elementoLista)
+      const botonBorrar = document.getElementById(`eliminarCarrito${id}`)
+      botonBorrar.addEventListener("click",()=>{
+          carrito = carrito.filter((elemento)=>{
+              if(elemento.id !== id){
+                  return elemento
+              }
+          })
+          let carritoString = JSON.stringify(carrito)
+          localStorage.setItem("carrito", carritoString)
+          mostrarCarrito()
+      })
+      let carritoString = JSON.stringify(carrito)
+      localStorage.setItem("carrito", carritoString)
+  })
+}
+
+const borrarCarrito = ()=>{
+  carrito.length = 0 
+  let carritoString = JSON.stringify(carrito)
+  localStorage.setItem("carrito", carritoString)
+  mostrarCarrito()
+}
+
+const productosPreexistentes = ()=>{
+
+  if (productos.length===0){
+      ProductoDefault.forEach(prod=>{
+          let ingreso = JSON.parse(JSON.stringify(prod))
+         agregarProductos(ingreso)}
+          )
+  }
+}
+
+const totalDelCarrito = ()=>{
+  let total = carrito.reduce((acumulador, {price, quantity})=>{
+      return acumulador + (price*quantity)
+  }, 0)
+  return total
+}
+const totalDelCarritoMostrar  = ()=>{
+  const carritoTotal = document.getElementById("carritoTotal")
+  carritoTotal.innerHTML=`<h2> $${totalDelCarrito() } </h2>`
+}
+
+
+
+
+const generarProductos = (arrayUtilizado)=>{
+
+    const contenedorProductos = document.getElementById("contenedorProductos")
+    contenedorProductos.innerHTML = ""
+    arrayUtilizado.forEach(({name, id, type, price, stock})=>{
+        const prodCard = document.createElement("div")
+        prodCard.classList.add("col-xs")
+        prodCard.classList.add("card")
+        
+        prodCard.id = id
+        prodCard.innerHTML = `
+
+        
+        <h5 class="card-titulo">${name}</h5>        
+        <img src="./images/${name+id}.webp" class="card-img-top" alt="${name}">
+                <div class="card-body">
+                    
+                    
+                <h5><span>$ ${price}</span></h5>
+                 
+        
+                    <form id="form${id} class="form-cant">
+                        <label class="p-2" for="contador${id}">
+                        QT</label>
+
+                        <input class="p-2 m-1" type="number" placeholder="0" id="contador${id}" >
+
+                        <button class="btn btn btn-dark m-3" id="botonProd${id}">
+                        Add to cart</button>
+                    </form>
+                </div>`
+        contenedorProductos.appendChild(prodCard)
+        const btn = document.getElementById(`botonProd${id}`)
+        
+        btn.addEventListener("click",(e)=>{
+          e.preventDefault()
+            const contadorQuantity = Number(document.getElementById(`contador${id}`).value)
+            if(contadorQuantity>0){
+                agregarCarrito({name, id, type, price, quantity:contadorQuantity})
+                mostrarCarrito()
+                const form = document.getElementById(`form${id}`)
+                form.reset()
+            }
+        }) 
+    })
+}
+const finalizarCompra = (event)=>{
+  const data = new FormData(event.target)
+  const cliente = Object.fromEntries(data)
+  const ticket = {
+    cliente: cliente, 
+    total:totalDelCarrito(),
+    id:compras.length, 
+    productos:carrito} 
+  compras.push(ticket)
+  localStorage.setItem("compras", JSON.stringify(compras))
+  
+  borrarCarrito()
+  let mensaje = document.getElementById("carritoTotal")
+  mensaje.innerHTML = "<h1>Thank you for buying</h1>"
+
+}
+
+
+const compraFinal = document.getElementById("formCompraFinal")
+compraFinal.addEventListener("submit",(event)=>{
+    
+    event.preventDefault()
+    if(carrito.length>0){
+        finalizarCompra(event)
+    } else {
+        
+    }
+})
+
+
+
+const ejecutarTienda = ()=>{
+    productosPreexistentes()
+    generarProductos(productos)
+    mostrarCarrito()
+    totalDelCarritoMostrar ()
+}
+
+
+ejecutarTienda()
 
